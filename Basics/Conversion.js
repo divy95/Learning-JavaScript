e1c276e2bfdae1cbf26ffa636c95bc99e219cd7c
divy95 <95542418+divy95@users.noreply.github.com>
2023-08-12 11:03:14 +0000

    Conversion

---
+++ Basics/Conversion.js
@@ -0,0 +1,7 @@
+// console.log("2">1);
+// console.log("02">1);
+// Typescript do not allow to compare two different data types
+console.log(null>0)//false Null is empty value. when i compared the value here javascript converted null to 0
+console.log(null==0);//false when i compared the value here javascript converted null to 0
+console.log(null >= 0);
+
