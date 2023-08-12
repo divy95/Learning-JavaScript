b27ec7b300697fd346ee0417e16306ba5c70df86
divy95 <95542418+divy95@users.noreply.github.com>
2023-08-12 09:34:00 +0000

    Conversion Of DataTypes

---
+++ Basics/_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score ="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let value=Number(score);
+// console.log(value);
+// console.log(typeof(value));
+// //"33"=>33
+//"33abc"=>NaN
+//"True"=>1, false=>0
+// ----Number to Boolean----
+// let isLoggedIn=1
+// let isLoggedIn=""
+// let booleanIsLoogedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+//1=>true; 0=>false
+//""=>false
+//"Divy"=true
+let someval=33;
+let someval1=String(someval);
+console.log(typeof someval1);
+console.log(someval1)
