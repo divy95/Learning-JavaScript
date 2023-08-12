45af0e97b501da3730f7305be35e882ecbc53d46
divy95 <95542418+divy95@users.noreply.github.com>
2023-08-12 10:50:54 +0000

    Operations

---
+++ Basics/Operation.js
@@ -0,0 +1,12 @@
+let str1="hello"
+let str2="Divy"
+let str3=str1+str2
+console.log(str3)
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + 2);
+console.log("1" + "2");
+console.log(1=="1")//== will only check the values but === will also check the data type
+console.log(1==="1")
+console.log(1+2+"3");
