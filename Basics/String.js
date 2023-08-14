ad75e410b783549116c16f14d0b1c0bd18a07fb1
divy95 <95542418+divy95@users.noreply.github.com>
2023-08-14 07:06:23 +0000

    Updated the code

--- Basics/String.js
+++ Basics/String.js
@@ -3,7 +3,7 @@ const repo=8
 // console.log(name + repo + "Value");
 console.log(`hello my name is ${name} and my repo count is ${repo}`);
 // One more method to declare an String
-const gameName=new String('Divy-Pratap')
+const gameName=new String('Divy-Pratap-Singh')
 //Key value pair access
 console.log(gameName[0]);
 //Accessing prototype
@@ -24,3 +24,19 @@ console.log(gameName.indexOf('v'));
 const newString=gameName.substring(0,4)
 // this will value from 0 to 3 except 4th value
 console.log(newString);
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);//this will give empty String
+// trim method
+const name1="    Divy      "
+// this will remove all the spaces from the String
+console.log(name1.trim());
+
+const url="https://Divy.com/divy%20pratap" //Now i want to remove this %from the url
+
+console.log(url.replace('%20','-'));
+
+// https://Divy.com/divy-pratap this will be the output
+console.log(url.includes('Divy'))
+const gameName2="Di-vy Pra-tap Singh";
+console.log(gameName2.split('-'));
+//Splitted the string basis on dash
